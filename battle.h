e0a8fa651b9fd9cad5585a37f5d2e090ef70b07f
L. Lawrence <45497330+lawrel@users.noreply.github.com>
2020-09-12 00:06:16 -0400

    Update battle.h

--- battle.h
+++ battle.h
@@ -2,8 +2,18 @@
 #include "pokedex.h"
 #include "structures.h"
 
+class Side {
+  public:
+    Side();
+  private:
+    vector<Pokemon> party;
+}
 class Battle {
   public:
     Battle();
+    void initFight();
   private:
+    string battle_type;
+    Side player;
+    Side opponent;
 };
